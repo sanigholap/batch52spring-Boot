cbf7b64924dd1862c6247e8ac69dc0aaa256d6b7
sani gholap <sanigholap51@gmail.com>
2025-04-11 18:47:06 +0530

    committing actuator project

---
+++ spring-boot-Acatutor-1/src/main/java/com/Controller/HomeController1.java
@@ -0,0 +1,17 @@
+package com.Controller;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Endpoint(id="new")
+public class HomeController1 {
+
+	@ReadOperation
+	public String getMes() {
+		
+		return "Hello I am java developer";
+	}
+}
---
+++ spring-boot-Acatutor-1/src/main/java/com/SpringBootAcatutor1Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAcatutor1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAcatutor1Application.class, args);
+	}
+
+}
